bc8e16a155f24e6431c3cac41f730094bafd2074
waimea-Jadavidson <168692078+waimea-Jadavidson@users.noreply.github.com>
2025-05-21 12:15:14 +1200

    [Commit] Rock Railing

--- app/__init__.py
+++ app/__init__.py
@@ -23,6 +23,12 @@ def home():
 
     return render_template("pages/home.jinja", things=records)
 
+@app.get("/thing/<int:id>")
+def showThing(id):
+    response = supabase.table("things").select().eq("id", id).single().execute()
+    record = response.data
+ 
+    return render_template("pages/thing.jinja", thing=record)
 
 @app.errorhandler(404)
 def notFound(error):
